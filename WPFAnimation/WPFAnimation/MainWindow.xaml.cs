0717c66d73a57ef973f824478c3d6c13eb8db663
endymion13 <francisco_mg13@hotmail.com>
2016-05-01 22:54:48 -0500

    First

---
+++ WPFAnimation/WPFAnimation/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFAnimation.Controls;
+
+
+namespace WPFAnimation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Events
+        public MainWindow()
+        {
+            InitializeComponent();      
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ClearAnimationPanel();
+
+                ctrlAnimation ctrlAnimationPanel = new ctrlAnimation();
+                pnlAnimations.Children.Add(ctrlAnimationPanel);
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        private void ClearAnimationPanel()
+        {
+            try
+            {
+                if (pnlAnimations.Children.Count > 0)
+                {
+                    pnlAnimations.Children.RemoveRange(0, pnlAnimations.Children.Count);
+                }
+             }
+            catch(Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+        }
+
+        #endregion
+
+
+    }
+}
